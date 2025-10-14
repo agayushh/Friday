8dd8d3d633297c3ccbd6624396f92d2dc358ed1d
agayushh <agayush088@gmail.com>
2025-10-14 20:12:06 +0530

    Add Navbar component with basic structure

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="m-2">
+      <ul className="flex justify-center space-x-12 border-y border-gray-900 py-3">
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
