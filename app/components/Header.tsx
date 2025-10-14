23956400d2f5c0c55b85a96181acea313f8ec2e7
agayushh <agayush088@gmail.com>
2025-10-14 20:12:00 +0530

    Add Header component and update Page structure

---
+++ app/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#09090b",
+        opacity: 1,
+        backgroundImage: "radial-gradient(#161616 0.8px, #09090b 0.8px)",
+        backgroundSize: "16px 16px",
+      }}
+      className="min-h-60  w-full flex justify-center items-center text-white"
+    >
+      <h1 className="text-9xl font-doto font-extralight">AG</h1>
+    </div>
+  );
+};
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Pfp from "./components/Pfp";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>Hey Friday!</div>
-  )
-}
+    <div className="flex justify-center w-screen my-2">
+      <div className="border-x h-screen border-gray-900 w-full sm:w-4/5 md:w-3/5 lg:w-1/2 xl:w-[42%]">
+        <Navbar />
+        <Header />
+        <Pfp />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Page;
