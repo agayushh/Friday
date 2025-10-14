577faf8e4bae7be9843b89b5f7877e6a57597f18
agayushh <agayush088@gmail.com>
2025-10-14 23:09:20 +0530

    Update layout styles to ensure full-screen body rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable}
-        ${doto.variable} antialiased`}
+        ${doto.variable} antialiased h-screen w-screen overflow-hidden`}
       >
         {children}
       </body>
