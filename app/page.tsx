594fbd167994a281afe3dedb3d7251f089f368bb
agayushh <agayush088@gmail.com>
2025-10-17 23:31:54 +0530

    Update Page component layout for improved structure and full-height rendering

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,21 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Pfp from "./components/Pfp";
 import PersonalInfo from "./components/PersonalInfo";
+import BorderSpacing from "./components/BorderSpacing";
+import Socials from "./components/Socials";
 
 const Page = () => {
   return (
     <div className="flex justify-center size-full">
-      <div className="border-x h-fit border-gray-900 w-11/12 sm:w-9/12 md:w-9/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12">
+      <div className="border-x h-screen border-gray-900 w-11/12 sm:w-9/12 md:w-9/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12">
         <Navbar />
         <Header />
         <Pfp />
+        <BorderSpacing />
         <PersonalInfo />
+        <BorderSpacing />
+        <Socials />
+
       </div>
     </div>
   );
