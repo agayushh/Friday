f7f6d98c8330a10ad97c57617b98455e3338a867
agayushh <agayush088@gmail.com>
2025-11-10 15:47:33 +0530

    stack loaded

---
+++ app/siteComponents/Stack.tsx
@@ -0,0 +1,22 @@
+import { TECH_STACK } from "../config/techStack";
+
+export default function Stack() {
+  return (
+    <div className="mt-10">
+      <div className="text-2xl pl-3">Stack</div>
+      <div className="border-t absolute w-screen left-0"></div>
+      <div className=" mt-5 mb-4 grid grid-cols-6 sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-10 xl:grid-cols-12 gap-4 m-1">
+        {TECH_STACK.map((stk) => {
+          return (
+            <div key={stk.key} className="h-10 w-10 mx-1">
+              <a href={stk.href}>
+                <img src={stk.icon} alt={stk.title} />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+      <div className="border-t absolute w-screen left-0"></div>
+    </div>
+  );
+}
