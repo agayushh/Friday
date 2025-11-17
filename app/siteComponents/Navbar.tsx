23c0e613af47d72285a161d1746876e63cfb44d5
agayushh <agayush088@gmail.com>
2025-11-17 20:09:52 +0530

    navbar ui changed

--- app/siteComponents/Navbar.tsx
+++ app/siteComponents/Navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Moon, Sun } from "lucide-react";
+import { FiMoon } from "react-icons/fi";
+import { LuSunMedium } from "react-icons/lu";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -27,19 +29,31 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed left-[-10px] flex justify-center items-center w-screen bg-white dark:bg-[#09090b] z-30 transition-colors duration-300">
-      <ul className="flex justify-center items-center mt-1.5 space-x-12 border-y border-gray-200 dark:border-gray-900 py-3 w-screen text-gray-900 dark:text-white">
-        <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
-        <button
-          onClick={toggleDarkMode}
-          className="ml-4 p-2 rounded-lg bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition-all duration-300"
-          aria-label="Toggle dark mode"
-        >
-          {darkMode ? <Sun size={20} /> : <Moon size={20} />}
-        </button>
-      </ul>
+    <div className="fixed left-0 flex justify-center items-center w-screen bg-white dark:bg-[#09090b] z-30 transition-colors duration-300">
+      <div className="flex justify-center items-center mt-1.5 space-x-12 border-y border-gray-200 dark:border-gray-900 py-3 w-screen text-gray-900 dark:text-white h-12">
+        <div className="w-[700px] flex justify-between items-center">
+          <div className="md:ml-5 ml-9 font-jura text-5xl"> AG</div>
+          <div className="flex ml-20 items-center">
+            <div className="font-jura flex justify-between mitems-center w-48">
+              <span>Portfolio</span>
+              <span>Projects</span>
+              <span>Blogs</span>
+            </div>
+            <span className="ml-4">
+              <a href="https://github.com/agayushh/friday">
+                <FaGithub size={18} />
+              </a>
+            </span>
+            <button
+              onClick={toggleDarkMode}
+              className="ml-4 px-3 rounded-lg transition-all duration-300"
+              aria-label="Toggle dark mode"
+            >
+              {darkMode ? <LuSunMedium size={18} /> : <FiMoon size={18} />}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
